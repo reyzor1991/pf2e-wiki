05007892c7dc0fd83862d25a93ba6fa8c07302d0
Mykhailo Budiakov <mishabudjakov@gmail.com>
2024-10-28 16:26:53 +0200

    simple deploy

--- data.js
+++ data.js
@@ -2,119 +2,119 @@ export const data = [
     {
         id: 1,
         title: 'PF2e Automations',
-        img: '/img/gear.svg',
+        img: '/pf2e-wiki/img/gear.svg',
         "description": "Module for automate action/feats/spells",
         "price": "free"
     },
     {
         id: 2,
         title: 'PF2e Hands Management (Reminder)',
-        img: '/img/hand.svg',
+        img: '/pf2e-wiki/img/hand.svg',
         "description": "Module to notify about hands management",
         "price": "free"
     },
     {
         id: 3,
         title: 'PF2e Party Sheet Helper',
-        img: '/img/group.svg',
+        img: '/pf2e-wiki/img/group.svg',
         "description": "Module for improving work with PF2e Party Sheet",
         "price": "free"
     },
     {
         id: 4,
         title: 'Pf2e Kingmaker Helper',
-        img: '/img/crown.svg',
+        img: '/pf2e-wiki/img/crown.svg',
         "description": "Module for improving work Kingmaker module",
         "price": "free"
     },
     {
         id: 5,
         title: 'PF2e Macros',
-        img: '/img/gear.svg',
+        img: '/pf2e-wiki/img/gear.svg',
         "description": "Module adds macros for improving action/feats",
         "price": "free"
     },
     {
         id: 6,
         title: 'PF2e Troops Helper',
-        img: '/img/group.svg',
+        img: '/pf2e-wiki/img/group.svg',
         "description": "Module for working with troops. Transformation/control",
         "price": "free"
     },
     {
         id: 7,
         title: 'PF2e Summons Helper',
-        img: '/img/pentagram.svg',
+        img: '/pf2e-wiki/img/pentagram.svg',
         "description": "Module for Summon/Dismiss/Sustain minions",
         "price": "free"
     },
     {
         id: 8,
         title: 'PF2e Reaction Checker',
-        img: '/img/gear.svg',
+        img: '/pf2e-wiki/img/gear.svg',
         "description": "Small module to notify that actor can use reaction",
         "price": "free"
     },
     {
         id: 9,
         title: 'PF2e Dying',
-        img: '/img/skull.svg',
+        img: '/pf2e-wiki/img/skull.svg',
         "description": "Module for handling dying conditions",
         "price": "free"
     },
     {
         id: 10,
         title: 'PF2e Weapon Trait Options',
-        img: '/img/sword.svg',
+        img: '/pf2e-wiki/img/sword.svg',
         "description": "Module add possibility to add missed modificators to attack/damage roll",
         "price": "free"
     },
     {
         id: 11,
         title: 'PF2e Weapon Trait Options',
-        img: '/img/trait.svg',
+        img: '/pf2e-wiki/img/trait.svg',
         "description": "Module add possibility to add missed modificators to attack/damage roll",
         "price": "free"
     },
     {
         id: 12,
         title: 'PF2e Gatewalkers Helper',
-        img: '/img/group.svg',
+        img: '/pf2e-wiki/img/group.svg',
         "description": "Module provide feature for handling Devian Feats",
         "price": "free"
     },
     {
         id: 13,
         title: 'PF2e Message Editor',
-        img: '/img/message.svg',
+        img: '/pf2e-wiki/img/message.svg',
         "description": "Module for editing messages in chat",
         "price": "free"
     },
     {
         id: 14,
         title: 'PF2e Initiative Effect',
-        img: '/img/2d6.svg',
+        img: '/pf2e-wiki/img/2d6.svg',
         "description": "Module for homebrew rule, apply effect when roll 1/20 on initiative",
         "price": "free"
     },
     {
         id: 15,
         title: 'PF2e Eidolon Helper',
-        img: '/img/pentagram.svg',
+        img: '/pf2e-wiki/img/pentagram.svg',
         "description": "Module for improving work with Eidolons",
         "price": "free"
     },
     {
         id: 16,
         title: 'PF2e Flip Token',
-        img: '/img/flip.svg',
+        img: '/pf2e-wiki/img/flip.svg',
         "description": "Module to flip token image",
         "price": "free"
     },
     {
         id: 17,
         title: 'PF2e Automations (Patreon)',
-        img: '/img/gear.svg',
+        img: '/pf2e-wiki/img/gear.svg',
         "description": "Module for automate action/feats/spells",
         "price": "patreon"
     }
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 String.prototype.capitalize = function () {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <HashRouter>
         <App/>
-    </BrowserRouter>,
+    </HashRouter>,
 )
